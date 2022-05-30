cebe58ae029af708d2552354bbff6689028d2954
nestoredi <86657718+nestoredi@users.noreply.github.com>
2022-05-30 20:18:08 -0300

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -3,12 +3,22 @@ fetch('pinturas.json')
 .then(response =>response.json())
 .then (pinturas =>{
     pinturas.forEach((pintura)=>{
-        let {id,nombre,autor,precio,stock}= pintura
+        let {id,nombre,autor,precio,stock,imagen}= pintura
         caja.innerHTML += `
-        <h3> Nombre: ${nombre}</h3>
+    <div class="divcard">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body position">
+      <img  src = "${imagen}">
+        <h5>${nombre}</h5>
         <p> Autor :${autor}</p>
         <p> Precio :${precio}</p>
-        <p> Stock :${stock}</p>
+        <p> Stock :${stock}</p></div>
+        <a href="./${id}.html" class="btn btn-primary " id="boton${id}">ver pintura</a>
+      </div>
+    </div>
+  </div>
+ 
         `
     })
 })
